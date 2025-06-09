56a3e400e697ba14a0c7e7fd298854ef817cec77
Zachary Kenneth Stewart <pro_zac@hotmail.com>
2025-06-09 14:21:47 +1000

    QTLseq_post fixes and updates
    Works with the new plotting classes, removes unnecessary code duplication in snpindex.py, and combines its remaining function into the QTLseq_post.py body for compactness

--- tests/QTLseq_post.py
+++ tests/QTLseq_post.py
@@ -6,15 +6,63 @@ import os, argparse, re, sys, pickle
 import numpy as np
 import matplotlib.pyplot as plt
 from Bio import SeqIO
-
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-from snpindex import DeltaNCLS, parse_qtlseq_as_dict, convert_dict_to_deltancls
+from pycirclize.parser import Gff
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from modules.plot import HorizontalPlot
+from modules.plot import HorizontalPlot, CircosPlot
 from modules.gff3 import GFF3
 from modules.validation import validate_regions
+from modules.parsing import read_gz_file
+from modules.ed import convert_dict_to_windowed_ncls
 
+# Plotting and parsing functions
+def parse_qtlseq_as_dict(qtlseqFile):
+    '''
+    Parameters:
+        qtlseqFile -- a string indicating the path to QTL-seq file
+    Returns:
+        deltaDict -- a dictionary with structure like:
+                     {
+                         "chr1": [[pos1, pos2, ...], [delta1, delta2, ...]],
+                         "chr2": [[pos1, pos2, ...], [delta1, delta2, ...]],
+                         ...
+                     }
+    '''
+    HEADER = ["CHROM", "POSI", "variant", "bulk1_alleles", "bulk2_alleles", "euclideanDist"]
+    
+    # Parse the ED file
+    deltaDict = {}
+    starts, ends = [], []
+    with read_gz_file(qtlseqFile) as fileIn:
+        firstLine = True
+        for line in fileIn:
+            sl = line.rstrip("\r\n").split("\t")
+            # Optionally skip header
+            "Not every QTL-seq index file has a header"
+            if firstLine:
+                firstLine = False
+                if sl[0] == "CHROM":
+                    continue
+            
+            # Parse relevant details and validate format
+            chrom, posi, variant, bulk1_depth, bulk2_depth, p99, p95, \
+                bulk1_SNPindex, bulk2_SNPindex, delta_SNPindex = sl
+            try:
+                posi = int(posi)
+            except:
+                raise ValueError(f"Position '{posi}' is not an integer; offending line is '{line}'")
+            try:
+                deltaValue = float(delta_SNPindex)
+            except:
+                raise ValueError(f"Delta SNP-index '{delta_SNPindex}' is not a float; offending line is '{line}'")
+            
+            # Store in dictionary
+            deltaDict.setdefault(chrom, [[], []])
+            deltaDict[chrom][0].append(posi)
+            deltaDict[chrom][1].append(abs(deltaValue))
+    return deltaDict
+
+# Validation functions
 def validate_args(args):
     # Validate input file
     if not os.path.isfile(args.inputFile):
@@ -27,10 +75,14 @@ def validate_args(args):
     # Validate annotation GFF3 file
     if args.annotationGFF3 != None:
         if not os.path.isfile(args.annotationGFF3):
-            raise FileNotFoundError(f"-a/--annotation file '{args.annotationGFF3}' is not a file!")
+            raise FileNotFoundError(f"--annotation file '{args.annotationGFF3}' is not a file!")
         else:
-            args.gff3Obj = GFF3(args.annotationGFF3) # parsing now to raise errors early
-            args.gff3Obj.create_ncls_index("gene")
+            if args.plotStyle == "horizontal":
+                args.gff3Obj = GFF3(args.annotationGFF3) # parsing now to raise errors early
+                args.gff3Obj.create_ncls_index("gene")
+            elif args.plotStyle == "circos":
+                parser = Gff(args.annotationGFF3)
+                args.gff3Obj = parser.get_seqid2features(feature_type=None)
     
     # Validate output file
     if os.path.exists(args.outputFileName):
@@ -50,10 +102,22 @@ def validate_p(args):
     if args.height != None:
         if args.height < 1:
             raise ValueError(f"--height value '{args.height}' must be >= 1!")
-    if args.binSize < 2:
-        raise ValueError(f"--bin value '{args.binSize}' must be >= 2!")
-    if args.binThreshold < 0:
-        raise ValueError(f"--threshold value '{args.binThreshold}' must be >= 0! (note that we use ABSOLUTE values)")
+    if args.axisSpace < 0:
+        raise ValueError(f"--axisSpace value '{args.axisSpace}' must be >= 0!")
+    if args.axisSpace >= 360:
+        raise ValueError(f"--axisSpace value '{args.axisSpace}' must be < 360!")
+    
+    # Alert user to Circos dimensions logic
+    if args.plotStyle == "circos":
+        if (args.width != None and args.height != None):
+            if args.width != 8 or args.height != 8:
+                print("# Note: psQTL is not yet equipped to handle non-default width or height while maintaining " +
+                      "clarity for Circos plots, so width and height will be set to 8.")
+                args.width = 8
+                args.height = 8
+            if args.width != args.height:
+                print(f"# Note: Circos plotting enforces square dimensions, so width and height " +
+                    f"will be set to the smallest value provided i.e., '{min(args.width, args.height)}'.")
     
     # Validate plot types
     if len(set(args.plotTypes)) != len(args.plotTypes):
@@ -62,8 +126,8 @@ def validate_p(args):
         raise ValueError(f"Cannot plot gene locations without providing an --annotation GFF3 file!")
     
     # Validate output file suffix
-    if not args.outputFileName.endswith(".pdf") and not args.outputFileName.endswith(".png"):
-        raise ValueError(f"-o output file '{args.outputFileName}' must end with '.pdf' or '.png'!")
+    if not (args.outputFileName.endswith(".pdf") or args.outputFileName.endswith(".png") or args.outputFileName.endswith(".svg")):
+        raise ValueError(f"-o output file '{args.outputFileName}' must end with '.pdf', '.png', or '.svg'!")
 
 def main():
     usage = """%(prog)s performs post-processing of QTL-seq output files to generate plots for comparison
@@ -83,11 +147,16 @@ def main():
     p.add_argument("-p", dest="plotTypes",
                    required=True,
                    nargs="+",
-                   choices=["line", "scatter", "histogram", "genes"],
+                   choices=["line", "scatter", "genes"],
                    help="Specify one or more plot types to generate")
+    p.add_argument("-s", dest="plotStyle",
+                   required=True,
+                   choices=["horizontal", "circos"],
+                   help="Specify the style of plot to generate")
     p.add_argument("-o", dest="outputFileName",
                    required=True,
-                   help="Specify the location to write the output file")
+                   help="""Specify the location to write the output file; this must
+                   end with '.pdf', '.png', or '.svg'""")
     # Set optional arguments
     ## File arguments
     p.add_argument("--annotation", dest="annotationGFF3",
@@ -102,19 +171,6 @@ def main():
                    values to consider during weighted moving average
                    calculation (default: 5)""",
                    default=5)
-    p.add_argument("--bin", dest="binSize",
-                   type=int,
-                   required=False,
-                   help="""HISTOGRAM PLOT: Optionally, specify the bin size to 
-                   count variants within (default: 100000)""",
-                   default=100000)
-    p.add_argument("--threshold", dest="binThreshold",
-                   type=float,
-                   required=False,
-                   help="""HISTOGRAM PLOT: Optionally, specify the ABSOLUTE delta
-                   SNP-index threshold for counting a variant within each bin
-                   (default: 0.4)""",
-                   default=0.4)
     ## Style arguments
     p.add_argument("--regions", dest="regions",
                    required=False,
@@ -138,6 +194,13 @@ def main():
                    help="""Optionally, specify the output plot height
                    (default: calculated internally with 5 per plot type)""",
                    default=None)
+    p.add_argument("--space", dest="axisSpace",
+                   type=int,
+                   required=False,
+                   help="""CIRCOS PLOT: Optionally, specify the space (in degrees)
+                   to allow for the Y-axis labels in the top centre of the plot
+                   (default: 10)""",
+                   default=10)
     
     args = p.parse_args()
     validate_args(args)
@@ -149,69 +212,40 @@ def main():
     # Get contig lengths from genome FASTA
     genomeRecords = SeqIO.parse(open(args.genomeFasta, 'r'), "fasta")
     lengthsDict = { record.id:len(record) for record in genomeRecords }
+    if lengthsDict == {}:
+        raise ValueError(f"No contigs found in genome FASTA '{args.genomeFasta}'; is it actually a FASTA file?")
     
     # Validate and impute regions
     args.regions = validate_regions(args, lengthsDict)
     
     # Parse input file into NCLS data structure
     deltaDict = parse_qtlseq_as_dict(args.inputFile)
-    deltaNCLS = convert_dict_to_deltancls(deltaDict)
-    
-    # Make sure all contigs in QTLseq data are in genome FASTA
-    for contigID in deltaNCLS.contigs:
-        if contigID not in lengthsDict:
-            raise ValueError(f"Contig ID '{contigID}' from QTL-seq data not found in -f FASTA!")
+    deltaNCLS = convert_dict_to_windowed_ncls(deltaDict)
     
     # Split into mode-specific functions
-    "There are no alternate modes with this script, just leaving it like this for consistency"
+    "There are no alternate modes with this script, just leaving it like this for consistency with psQTL_post.py"
     pmain(args, deltaNCLS, lengthsDict)
     
     # Print completion flag if we reach this point
     print("Program completed successfully!")
 
 def pmain(args, deltaNCLS, lengthsDict):
-    args.inputType = ["call"]
-    args.measurementTypes = ["ed"]
-    
     # Establish plotting object
-    plotter = HorizontalPlot(args.inputType, args.measurementTypes, args.plotTypes, args.regions,
-                             args.wmaSize, args.binSize, args.binThreshold,
-                             args.width, args.height)
-    plotter.start_plotting()
-    
-    rowLabels = []
-    colLabels = []
-    
-    # Generate plots grouped by plotTypes > inputType > measurementTypes
-    alreadyPlottedScatter = False
-    for pType in args.plotTypes:
-        # Plot a line and/or scatter plot
-        if pType in ["line", "scatter"] and not alreadyPlottedScatter:
-            alreadyPlottedScatter = True
-            rowLabels.append(f"abs. ΔSNP-index")
-            plotter.plot_linescatter(deltaNCLS, deltaNCLS)
-        # Plot a histogram
-        elif pType == "histogram":
-            rowLabels.append(f"Num. variants with abs. ΔSNP-index ≥ {args.binThreshold}\n" + \
-                f"in {args.binSize} bp windows")
-            plotter.plot_histogram(deltaNCLS)
-        # Plot gene locations
-        elif pType == "genes":
-            plotter.plot_genes(args.gff3Obj)
-            rowLabels.append("Representative models")
-    
-    # Format column labels
-    colLabels = [f"{region[0]}:{region[1]}-{region[2]}" if region[3] == False
-                 else f"{region[0]}:{region[2]}-{region[1]}" # if reversed
-                 for region in args.regions]
-    
-    # Set the row and column labels
-    plotter.set_row_labels(rowLabels)
-    plotter.set_col_labels(colLabels)
-    
-    # Write plot to file
-    plotter.savefig(args.outputFileName)
-    
+    if args.plotStyle == "horizontal":
+        plotter = HorizontalPlot(args.regions, callED=deltaNCLS, depthED=None,
+                                 callSPLSDA=None, depthSPLSDA=None, integratedSPLSDA=None,
+                                 annotationGFF3=args.gff3Obj if "genes" in args.plotTypes else None,
+                                 power=1, wmaSize=args.wmaSize, width=args.width, height=args.height,
+                                 coverageSamples=None)
+        plotter.plot(args.plotTypes, args.outputFileName)
+    elif args.plotStyle == "circos":
+        plotter = CircosPlot(args.regions, callED=deltaNCLS, depthED=None,
+                                 callSPLSDA=None, depthSPLSDA=None, integratedSPLSDA=None,
+                                 annotationGFF3=args.gff3Obj if "genes" in args.plotTypes else None,
+                                 power=1, wmaSize=args.wmaSize, width=args.width, height=args.height,
+                                 coverageSamples=None)
+        plotter.axisSpace = args.axisSpace
+        plotter.plot(args.plotTypes, args.outputFileName)
     print("Plotting complete!")
 
 if __name__ == "__main__":
